e275ede7508608c4fdd78d8c0e72768008601696
Ajay Kumar <av0270084@gmail.com>
2025-12-06 10:27:14 +0530

    feat: add routes for user channel profile and watch history retrieval

--- src/controllers/user_controller.js
+++ src/controllers/user_controller.js
@@ -468,6 +468,8 @@ export{
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
 
 }
